ab30860757f8fabf8d8621245d59c819de123825
Saralytics <zhaoxue.li@outlook.com>
2023-07-18 22:19:49 +0400

    first full wordcount

---
+++ eclipse-workspace/MapReduceCodeExamples/src/demo/wordcount/WordCountMain.java
@@ -0,0 +1,42 @@
+package demo.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.Path;
+
+
+public class WordCountMain {
+	// This is more or less a template. All Mapreduce main programs follow this structure. 
+
+	public static void main(String[] args) throws Exception {
+		// 1. create a job, and specify entrypoint
+		
+		Job job = Job.getInstance(new Configuration());
+		job.setJarByClass(WordCountMain.class);
+		
+		// 2. Specify Map and Map output type 
+		job.setMapperClass(WordCountMapper.class);
+		job.setMapOutputKeyClass(Text.class); // k2
+		job.setMapOutputValueClass(IntWritable.class); //v2
+		
+		// 3. Specify Reduce and Reduce output type 
+		
+		job.setReducerClass(WordCountReducer.class); 
+		job.setOutputKeyClass(Text.class); //k4
+		job.setOutputValueClass(IntWritable.class); // v4
+		
+		// 4. Specify Job input and output paths 
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		
+		// 5. Run job
+		job.waitForCompletion(true); // true: print logs 
+
+	}
+
+}
--- eclipse-workspace/MapReduceCodeExamples/src/demo/wordcount/WordCountMapper.java
+++ eclipse-workspace/MapReduceCodeExamples/src/demo/wordcount/WordCountMapper.java
@@ -10,10 +10,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	@Override
-	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+	protected void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		super.map(key, value, context);
+		
+		// Read in the input and tokenize the line
+		String input = value.toString();
+		String[] words = input.split(" ");
+		
+		// for each word, the k2 is the word itself, v2 is 1 
+		for (String w:words) {
+			context.write(new Text(w), new IntWritable(1));
+		}
+		
 	}
 
 }
